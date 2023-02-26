188f70330d520935d6fb9455f7dae79699fcf503
nareshkumarailaveni <ailaveninareshkumar@gmail.com>
2023-02-26 09:58:28 +0530

    Changed from Add to Sub

--- Data_Processing.py
+++ Data_Processing.py
@@ -3,7 +3,7 @@ print("adding all the data processing ccode related fraud detection")
 a-5
 b=7
 def sum(a,b)
-    c=a+b
+    c=a-b
     retrun a
 
 print(" Added a new function sum which is not correct")
